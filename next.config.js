f3daa1babb01991097b0fb47297365aa7e47b02d
Naveen K <140600164+naveenkkannan@users.noreply.github.com>
2024-09-30 15:44:16 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const { withStoreConfig } = require("./store-config")
 const store = require("./store.config.json")
 // next.config.js
 const path = require('path');
+require('dotenv').config();
 
 /**
  * @type {import('next').NextConfig}
@@ -38,7 +39,12 @@ console.log("next.config.js", JSON.stringify(module.exports, null, 2))
 module.exports = {
   // This tells Next.js where to find your app directory
   srcDir: path.join(__dirname, 'src'),
+env: {
+        MEDUSA_BACKEND_URL: process.env.MEDUSA_BACKEND_URL,
+    },
+
 };
 
 
+    
 module.exports = nextConfig
